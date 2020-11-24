ef6c4e2a07c668d18d5e5f4f2a6efccd02fc6d58
Tomasz Jagas <tomasz.jagas@gmail.com>
2020-11-24 21:25:49 +0100

    Add project files.

---
+++ audio-programming-012-expdecay/main.c
@@ -0,0 +1,34 @@
+/***********************************************
+* Descrition: implement formula x[t]=a*exp(-k/T)
+************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char** argv[]) {
+	int i, nsteps;
+	double step, x, a, T, k;
+	double dur;
+
+	if (argc != 4) {
+		printf("usage: expdecay dur T steps\n");
+		return 1;
+	}
+
+	dur = atof(argv[1]);
+	T = atof(argv[2]);
+	nsteps = atoi(argv[3]);
+
+	k = dur / nsteps; // constant time increment
+	a = exp(-k / T); // calculate constant ratio value
+	x = 1.0; // starting value for the decay
+	step = 0.0;
+
+	for (i = 0; i < nsteps; i++) {
+		printf("%.4lf\t%.8lf\n", step, x);
+		x = a * x;
+		step += k;
+	}
+	return 0;
+}
